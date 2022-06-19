48f9c0c92a106203486c1ee9b05b0dbb69ab45fb
Hedgehogo <lologlek8@gmail.com>
2022-06-19 18:38:19 +0300

    add debug interaction

--- interaction/iInteraction/mouse/lambda/mouseLambdaInteraction.h
+++ interaction/iInteraction/mouse/lambda/mouseLambdaInteraction.h
@@ -4,6 +4,8 @@
 namespace ui {
 	class MouseLambdaInteraction : public MouseInteraction, public LambdaInteraction {
 	public:
+		static MouseLambdaInteraction debug;
+		
 		MouseLambdaInteraction(ui::ButtonEvent *leftMouseButton, ui::ButtonEvent *rightMouseButton, void ( *startPointing)(sf::Vector2i mousePosition), void (*finishPointing)(sf::Vector2i mousePosition));
 		
 		~MouseLambdaInteraction() override;
--- main.cpp
+++ main.cpp
@@ -114,22 +114,7 @@ int main() {
 		},
 		InteractionStack {
 			std::vector<ui::IInteraction *> {
-				new ui::MouseLambdaInteraction {
-					new ui::ButtonEvent_LambdaSimple {
-						[](sf::Vector2i) {std::cout << "sl";},
-						[](sf::Vector2i) {std::cout << "wl";},
-						[](sf::Vector2i) {std::cout << "sl";},
-						[](sf::Vector2i) {std::cout << "nl";}
-					},
-					new ui::ButtonEvent_LambdaSimple {
-						[](sf::Vector2i) {std::cout << "sr" << std::endl;},
-						[](sf::Vector2i) {std::cout << "wr" << std::endl;},
-						[](sf::Vector2i) {std::cout << "sr" << std::endl;},
-						[](sf::Vector2i) {std::cout << "nr" << std::endl;}
-					},
-					[](sf::Vector2i) {std::cout << "sp" << std::endl;},
-					[](sf::Vector2i) {std::cout << "fp" << std::endl;}
-				},
+				ui::MouseLambdaInteraction::debug.copy()
 			}
 		},
 		window
