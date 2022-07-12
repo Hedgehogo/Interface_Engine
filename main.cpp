ba37764699ca8a298c1700aff3bbed05f29cf309
Hedgehogo <lologlek8@gmail.com>
2022-07-12 22:32:28 +0300

    Merge Sizing MinSize update into Panel

--- main.cpp
+++ main.cpp
@@ -16,122 +16,84 @@ int main() {
 	ui::Caption::setDefaultColor(sf::Color::White);
 	ui::Caption::setDefaultSize(15);
 	
-	ui::Interface interface {
-		new ui::LayerWithMovableBorder{
-			new ui::LayerWithMovableBorder{
-				new ui::LayerWithBorderHorizontal{
-					{
-						new ui::FullColor{{22, 22, 22}},
-						new ui::LayerWithConstRatio{
-							new ui::LayerWithAlternativeObject{
-                                new ui::ButtonWithPanel{
-                                    new ui::Panel{
-                                        new ui::Text{
-                                            {
-                                                new ui::TextBlock{
-                                                    L"это собственность Артёма"
-                                                }
-                                            },
-                                            new ui::RoundedRectangle{sf::Color(0xffffffff), 10},
-                                            14,
-                                            1.15,
-                                            &font,
-                                            sf::Color{10, 10, 0},
-                                            sf::Color{200, 200, 200},
-                                            sf::Color{200, 200, 200},
-                                            ui::Text::Align::center
-                                        }
-                                        /*new ui::FullColor{sf::Color(0xffffffff)}*/,
-                                        new ui::PointingHidePanelInteraction,
-                                        new ui::Sizing2{
-                                            sf::Vector2f{1, 0.5},
-                                            sf::Vector2f{}
-                                        },
-                                        new ui::Positioning2{
-                                            ui::Location2::right,
-                                            ui::Location2::left
-                                        }
-                                    },
-                                    new ui::PointingDisplayPanelInteraction,
-                                    new ui::LayerWithRenderTexture{
-                                        new ui::Sprite{texture_2}
-                                    }
-                                },
-								new ui::LayerWithBorderVertical{
-									new ui::Empty,
-									new ui::FullColor{sf::Color(0x9f6e5cff)}
-								},
-							},
-							new ui::FullColor{{22, 22, 22}},
-							new ui::FullColor{{22, 22, 22}},
-							1,
-							ui::Corner::DownLeft
-						},
-						new ui::FullColor{{22, 22, 22}}
-					},
-					std::vector<float>{
-						0.4,
-						0.6
-					}
-				},
-				new ui::LayerWithBorderHorizontal{
-					{
-						new ui::FullColor{{22, 22, 22}},
-						new ui::Text{
-							{
-								new ui::TextBlock{
-									L"Box2D is a 2D rigid body simulation library for games. Programmers can use it in their games to make objects move in realistic ways and make the game world more interactive. From the game engine's point of view, a physics engine is just a system for procedural animation. \nBox2D is written in portable C++. Most of the types defined in the engine begin with the b2 prefix. Hopefully this is sufficient to avoid name clashing with your game engine."
-								}
-							},
-							new ui::FullColor{sf::Color(0x9f6e5cff)},
-							14,
-							1.15,
-							&font,
-							sf::Color{10, 10, 0},
-							sf::Color{200, 200, 200},
-							sf::Color{200, 200, 200},
-							ui::Text::Align::center
-						},
-						new ui::FullColor{{22, 22, 22}}
-					},
-					std::vector<float>{
-						0.4,
-						0.6
-					}
-				},
-				false
-			},
-			new ui::LayerWithConstCenter{
-				new ui::LayerWithAlternativeObject{
-					new ui::LayerWithBorderHorizontal{
-						{
-							new ui::Capsule{sf::Color(0x9f6e5cff)},
-							new ui::Capsule{sf::Color(0x9f6e5cff)}
-						},
-						{   0.5}
-					},
-					new ui::LayerWithBorderVertical{
-						{
-							new ui::LayerWithBorderHorizontal{
+	ui::ButtonWithPanel* buttonWithPanel {
+		new ui::ButtonWithPanel {
+			new ui::Panel {
+				new ui::ButtonWithPanel {
+					new ui::Panel {
+						new ui::LayerWithConstBezel {
+							new ui::Text{
 								{
-									new ui::Empty,
-									new ui::FullColor{sf::Color(0x9f6e5cff)},
-									new ui::Empty
+									new ui::InteractiveTextBlock{
+										0,
+										L"соси жопу грёбаный убюдок мать твою, иди сюда блять"
+									}
 								},
-								std::vector<float>{
-									0.25,
-									0.75
-								}
+								new ui::FullColor{{60, 63, 65}},
+								14,
+								1.15,
+								&font,
+								sf::Color::Green
 							},
-							new ui::Empty
+							new ui::FullColor{{81, 81, 81}},
+							1
+						},
+						new ui::PointingHidePanelInteraction{},
+						new ui::Sizing2{new ui::RelativeParentSizing{}, new ui::ConstSizing{100}},
+						new ui::Positioning2{ui::Location2::downLeft, ui::Location2::upLeft, {0, -1}}
+					},
+					new ui::PointingDisplayPanelInteraction{},
+					new ui::LayerWithPanel{
+						new ui::LayerWithConstBezel{
+							new ui::FullColor{{60, 63, 65}},
+							new ui::FullColor{{81, 81, 81}}, 1
+						},
+						new ui::ConstPanel{
+							new ui::LayerWithConstBezel{
+								new ui::FullColor{{60, 63, 65}},
+								new ui::FullColor{{81, 81, 81}}, 1
+							},
+							new ui::Sizing2{sf::Vector2f{0.5, 0.5}, sf::Vector2f{}},
+							new ui::Positioning2{ui::Location2::upRight, ui::Location2::upLeft}
 						}
 					}
 				},
-				new ui::FullColor{{22, 22, 22}},
-				2
+				new ui::ClickHidePanelInteraction{sf::Mouse::Left},
+				new ui::Sizing2{true},
+				new ui::Positioning2{ui::Location2::upRight, ui::Location2::upLeft, {-5, 5}}
 			},
-			false,
-			0.75
+			new ui::PointingDisplayPanelInteraction{},
+			new ui::FullColor{{78, 82, 84}}
+		},
+	};
+	
+	ui::Interface interface {
+		new ui::LayerWithConstBorder {
+			new ui::Slider {
+				new ui::Capsule({78, 78, 78}),
+				new ui::FullColor({43, 43, 43}),
+				sf::Vector2f{1.0f, 0.5f}
+			},
+			new ui::LayerWithConstRatio {
+				new ui::LayerWithConstBezel {
+					new ui::LayerWithConstBorder {
+						new ui::LayerWithBorderVertical {
+							{
+								buttonWithPanel,
+								new ui::FullColor{{60, 63, 65}},
+								buttonWithPanel->copy()
+							}
+						},
+						new ui::FullColor{{60, 63, 65}},
+						ui::Side::Up, 81
+					},
+					new ui::FullColor{{50, 50, 50}}, 1
+				},
+				new ui::FullColor{{43, 43, 43}},
+				new ui::FullColor{{43, 43, 43}},
+				1.f / 3.f, ui::Corner::UpLeft
+			},
+			ui::Side::Right, 10
 		},
 		new ui::InteractionStack {
 			std::vector<ui::IInteraction *> {
