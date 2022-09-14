6087a8925f7cf8733930528743e11bde78982efb
Rin <jabkakek3@gmail.com>
2022-09-14 17:14:11 +0300

    LayerWithConstCenter update - Added 2 objects on the sides

--- lib/ui/iObject/iUnscalable/iScalable/layer/constCenter/layerWithConstCenter.cpp
+++ lib/ui/iObject/iUnscalable/iScalable/layer/constCenter/layerWithConstCenter.cpp
@@ -4,26 +4,54 @@ namespace ui {
 	void LayerWithConstCenter::init(sf::RenderTarget &renderTarget, DrawManager &drawManager, UpdateManager &updateManager, InteractionManager &interactionManager, InteractionStack &interactionStack, PanelManager &panelManager) {
 		background->init(renderTarget, drawManager, updateManager, interactionManager, interactionStack, panelManager);
 		object->init(renderTarget, drawManager, updateManager, interactionManager, interactionStack, panelManager);
+		drawManager.add(*this);
+		firstObject->init(renderTarget, this->firstDrawManager, updateManager, interactionManager, interactionStack, panelManager);
+		secondObject->init(renderTarget, this->secondDrawManager, updateManager, interactionManager, interactionStack, panelManager);
 	}
 	
 	LayerWithConstCenter::LayerWithConstCenter(IScalable* object, IUninteractive* background, float aspectRatio, sf::Vector2f minSize) :
-		Layer(minSize), LayoutWithObject(object), LayoutWithBackground(background), aspectRatio(aspectRatio) {}
+		Layer(minSize), LayoutWithObject(object), LayoutWithBackground(background), LayoutWithTwoObjects(new Empty, new Empty), aspectRatio(aspectRatio) {}
+
+	LayerWithConstCenter::LayerWithConstCenter(IScalable *object, IScalable *firstObject, IScalable *secondObject, IUninteractive *background, float aspectRatio, sf::Vector2f minSize) :
+		Layer(minSize), LayoutWithObject(object), LayoutWithBackground(background), LayoutWithTwoObjects(firstObject, secondObject), aspectRatio(aspectRatio) {}
 	
 	void LayerWithConstCenter::setAspectRatio(float aspectRatio) {
 		this->aspectRatio = aspectRatio;
 		this->setPosition(position);
 	}
-	
+
+	void LayerWithConstCenter::draw()
+	{
+		if ( renderFirst ) firstDrawManager.draw();
+		if ( renderSecond ) secondDrawManager.draw();
+	}
+
 	void LayerWithConstCenter::resize(sf::Vector2f size, sf::Vector2f position) {
 		Layer::resize(size, position);
+
+		sf::Vector2f firstMinSize { firstObject->getMinSize() };
+		sf::Vector2f secondMinSize { secondObject->getMinSize() };
+		sf::Vector2f secondPosition { position };
+		sf::Vector2f objectsSize { size };
+
 		background->resize(size, position);
 		float sizeAspectRatio = size.x / size.y;
 		if(sizeAspectRatio > aspectRatio) {
 			size.x = size.y * aspectRatio;
+			objectsSize.x = (this->size.x - size.x) / 2.0f;
+			secondPosition.x += objectsSize.x + size.x;
 		} else {
 			size.y = size.x / aspectRatio;
+			objectsSize.y = (this->size.y - size.y) / 2.0f;
+			secondPosition.y += objectsSize.y + size.y;
 		}
+		renderFirst = firstMinSize.x < objectsSize.x && firstMinSize.y < objectsSize.y;
+		renderSecond = secondMinSize.x < objectsSize.x && secondMinSize.y < objectsSize.y;
+
 		object->resize(size, position + (this->size - size) / 2.0f);
+
+		if (renderFirst) firstObject->resize(objectsSize, position);
+		if (renderSecond) secondObject->resize(objectsSize, secondPosition);
 	}
 	
 	bool LayerWithConstCenter::updateInteractions(sf::Vector2f mousePosition) {
@@ -46,18 +74,30 @@ namespace ui {
 	}
 	
 	LayerWithConstCenter *LayerWithConstCenter::copy() {
-		LayerWithConstCenter* layerWithConstCenter{new LayerWithConstCenter{object->copy(), background->copy(), aspectRatio, minimumSize}};
+		LayerWithConstCenter* layerWithConstCenter{new LayerWithConstCenter{object->copy(), firstObject->copy(), secondObject->copy(), background->copy(), aspectRatio, minimumSize}};
 		Layer::copy(layerWithConstCenter);
 		return layerWithConstCenter;
 	}
 	
 	LayerWithConstCenter *LayerWithConstCenter::createFromYaml(const YAML::Node &node) {
 		IScalable *object;
+		IScalable *firstObject;
+		IScalable *secondObject;
 		float aspectRatio;
 		IUninteractive *background{};
 		sf::Vector2f minSize{};
 		
 		node["object"] >> object;
+		if (node["first-object"]) {
+			node["first-object"] >> firstObject;
+		} else {
+			firstObject = new Empty{};
+		}
+		if (node["second-object"]) {
+			node["second-object"] >> secondObject;
+		} else {
+			secondObject = new Empty{};
+		}
 		node["aspect-ratio"] >> aspectRatio;
 		if(node["background"]) {
 			node["background"] >> background;
@@ -67,11 +107,14 @@ namespace ui {
 		if(node["min-size"])
 			node["min-size"] >> minSize;
 		
-		return new LayerWithConstCenter{object, background, aspectRatio, minSize};
+		return new LayerWithConstCenter{object, firstObject, secondObject, background, aspectRatio, minSize};
 	}
 	
 	void LayerWithConstCenter::drawDebug(sf::RenderTarget &renderTarget, int indent, int indentAddition, uint hue, uint hueOffset) {
         background->drawDebug(renderTarget, indent, indentAddition, hue, hueOffset);
         object->drawDebug(renderTarget, indent + indentAddition, indentAddition, hue + hueOffset, hueOffset);
+		firstObject->drawDebug(renderTarget, indent + indentAddition, indentAddition, hue + hueOffset, hueOffset);
+		secondObject->drawDebug(renderTarget, indent + indentAddition, indentAddition, hue + hueOffset, hueOffset);
 	}
+
 }
--- lib/ui/iObject/iUnscalable/iScalable/layer/constCenter/layerWithConstCenter.hpp
+++ lib/ui/iObject/iUnscalable/iScalable/layer/constCenter/layerWithConstCenter.hpp
@@ -5,17 +5,25 @@
 #include "../../uninteractive/onlyDrawable/empty/empty.hpp"
 #include "../../../../layout/object/layoutWithObject.hpp"
 #include "../../../../layout/background/layoutWithBackground.hpp"
+#include "../../../../layout/twoObjects/layoutWithTwoObjects.hpp"
+#include "../../uninteractive/onlyDrawable/empty/empty.hpp"
 
 namespace ui {
-	class LayerWithConstCenter : public Layer, public LayoutWithObject, public LayoutWithBackground {
+	class LayerWithConstCenter : public Layer, public LayoutWithObject, public LayoutWithBackground, public LayoutWithTwoObjects, public IDrawable {
 	protected:
 		float aspectRatio;
+		bool renderFirst;
+		bool renderSecond;
+		DrawManager firstDrawManager;
+		DrawManager secondDrawManager;
 	
 	public:
 		void init(sf::RenderTarget &renderTarget, DrawManager &drawManager, UpdateManager &updateManager, InteractionManager &interactionManager, InteractionStack &interactionStack, PanelManager &panelManager) override;
-		
+
 		LayerWithConstCenter(IScalable *object, IUninteractive *background, float aspectRatio, sf::Vector2f minSize = {});
-		
+
+		LayerWithConstCenter(IScalable *object, IScalable *firstObject, IScalable *secondObject, IUninteractive *background, float aspectRatio, sf::Vector2f minSize = {});
+
 		void setAspectRatio(float aspectRatio);
 		
 		void resize(sf::Vector2f size, sf::Vector2f position) override;
@@ -29,6 +37,8 @@ namespace ui {
 		LayerWithConstCenter* copy() override;
 		
 		static LayerWithConstCenter* createFromYaml(const YAML::Node &node);
+
+		void draw() override;
 		
 		void drawDebug(sf::RenderTarget &renderTarget, int indent, int indentAddition, uint hue, uint hueOffset) override;
 	};
