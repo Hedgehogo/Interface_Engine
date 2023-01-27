7013e1f9dacbe091ccdfe0bf830d63a041da00e9
Hedgehogo <lologlek8@gmail.com>
2023-01-28 00:37:11 +0300

    Switch update - Switch now supports shared variables

--- lib/ui/iObject/iUnscalable/iScalable/interactive/simple/switch/switch.cpp
+++ lib/ui/iObject/iUnscalable/iScalable/interactive/simple/switch/switch.cpp
@@ -2,94 +2,107 @@
 #include "../../../../../../interaction/iInteraction/oneKey/oneKeyInteraction.hpp"
 #include "event/switchEvent.hpp"
 
-namespace ui{
-
-    Switch::Switch(IUninteractive *inactiveBackground, IUninteractive *activeBackground, Key key, bool startActive) :
-		Interactive_Simple(new OneKeyInteraction{new SwitchEvent{*this}, key}), activeBackground(activeBackground), inactiveBackground(inactiveBackground), active(startActive){}
-
-    void Switch::init(InteractiveInitInfo interactiveInitInfo) {
+namespace ui {
+	Switch::Switch(IUninteractive *inactiveBackground, IUninteractive *activeBackground, PSbool value, Key key) :
+		Interactive_Simple(new OneKeyInteraction{new SwitchEvent{*this}, key}), activeBackground(activeBackground), inactiveBackground(inactiveBackground), active(value) {}
+	
+	Switch::Switch(IUninteractive *inactiveBackground, IUninteractive *activeBackground, Key key, bool startActive) :
+		Switch(inactiveBackground, activeBackground, std::make_shared<Sbool>(), key) {
+		active->setValue(startActive);
+	}
+	
+	void Switch::init(InteractiveInitInfo interactiveInitInfo) {
 		Interactive_Simple::init(interactiveInitInfo);
 		interactiveInitInfo.drawManager.add(*this);
 		activeBackground->init(interactiveInitInfo.copy(activeDrawManager).toGeneral(*interactionManager, *interactionStack));
 		inactiveBackground->init(interactiveInitInfo.copy(inactiveDrawManager).toGeneral(*interactionManager, *interactionStack));
-    }
-
-    void Switch::switchValue() {
-        active = !active;
-    }
-
-    void Switch::setPosition(sf::Vector2f position) {
-        Layout::setPosition(position);
-        activeBackground->setPosition(position);
-        inactiveBackground->setPosition(position);
-    }
-
-    void Switch::move(sf::Vector2f position) {
-        Layout::move(position);
-        activeBackground->move(position);
-        inactiveBackground->move(position);
-    }
-
-    void Switch::setSize(sf::Vector2f size) {
-        Layout::setSize(size);
-        activeBackground->setSize(size);
-        inactiveBackground->setSize(size);
-    }
-
-    sf::Vector2f Switch::getMinSize() {
-        return max(activeBackground->getMinSize(), inactiveBackground->getMinSize());
-    }
-
-    sf::Vector2f Switch::getNormalSize() {
-        return max(activeBackground->getNormalSize(), inactiveBackground->getNormalSize());
-    }
-    void Switch::draw() {
-        if(active)
-            activeDrawManager.draw();
-        else
-            inactiveDrawManager.draw();
-    }
-
-    void Switch::resize(sf::Vector2f size, sf::Vector2f position) {
-        Layout::resize(size, position);
-        activeBackground->resize(size, position);
-        inactiveBackground->resize(size, position);
-    }
-
-    Switch *Switch::copy() {
-        Switch* switcher{new Switch(inactiveBackground->copy(), activeBackground->copy(), dynamic_cast<OneKeyInteraction*>(interaction)->getKey(), active)};
-        Layout::copy(switcher);
-        return switcher;
-    }
+	}
+	
+	void Switch::switchValue() {
+		active->setValue(!active->getValue());
+	}
+	
+	void Switch::setPosition(sf::Vector2f position) {
+		Layout::setPosition(position);
+		activeBackground->setPosition(position);
+		inactiveBackground->setPosition(position);
+	}
+	
+	void Switch::move(sf::Vector2f position) {
+		Layout::move(position);
+		activeBackground->move(position);
+		inactiveBackground->move(position);
+	}
+	
+	void Switch::setSize(sf::Vector2f size) {
+		Layout::setSize(size);
+		activeBackground->setSize(size);
+		inactiveBackground->setSize(size);
+	}
+	
+	sf::Vector2f Switch::getMinSize() {
+		return max(activeBackground->getMinSize(), inactiveBackground->getMinSize());
+	}
+	
+	sf::Vector2f Switch::getNormalSize() {
+		return max(activeBackground->getNormalSize(), inactiveBackground->getNormalSize());
+	}
+	
+	void Switch::draw() {
+		if(active->getValue()) {
+			activeDrawManager.draw();
+		} else {
+			inactiveDrawManager.draw();
+		}
+	}
+	
+	void Switch::resize(sf::Vector2f size, sf::Vector2f position) {
+		Layout::resize(size, position);
+		activeBackground->resize(size, position);
+		inactiveBackground->resize(size, position);
+	}
+	
+	Switch *Switch::copy() {
+		Switch *switcher{new Switch(inactiveBackground->copy(), activeBackground->copy(), active, dynamic_cast<OneKeyInteraction *>(interaction)->getKey())};
+		Layout::copy(switcher);
+		return switcher;
+	}
 	
 	bool convertPointer(const YAML::Node &node, Switch *&switcher) {
-		IUninteractive *inactiveBackground;
-		IUninteractive *activeBackground;
-		Key key{Key::mouseLeft};
-		bool startActive{false};
+		auto inactiveBackground{node["inactive-background"].as<IUninteractive *>()};
+		auto activeBackground{node["active-background"].as<IUninteractive *>()};
+		Key key{convDef<Key>(node["key"], Key::mouseLeft)};
 		
-		node["inactive-background"] >> inactiveBackground;
-		node["active-background"] >> activeBackground;
-		if(node["key"])
-			node["key"] >> key;
-		if(node["state"]) {
-			startActive = convertBool(node["state"], "active", "inactive");
-		} else if(node["start-active"]) {
-			node["start-active"] >> startActive;
+		if(node["value"]) {
+			switcher = new Switch {
+				inactiveBackground, activeBackground,
+				Buffer::get<Sbool>(node["value"]),
+				key
+			};
+		} else if(node["state"]) {
+			switcher = new Switch {
+				inactiveBackground, activeBackground, key,
+				convertBool(node["state"], "active", "inactive")
+			};
+		} else {
+			switcher = new Switch {
+				inactiveBackground, activeBackground, key,
+				convDef(node["start-active"], false)
+			};
 		}
-		
-		{ switcher = new Switch{inactiveBackground, activeBackground, key, startActive}; return true; }
+		return true;
 	}
-
-    void Switch::drawDebug(sf::RenderTarget &renderTarget, int indent, int indentAddition, uint hue, uint hueOffset) {
-        if(active)
-            activeBackground->drawDebug(renderTarget, indent, indentAddition, hue, hueOffset);
-        else
-            inactiveBackground->drawDebug(renderTarget, indent, indentAddition, hue, hueOffset);
-    }
-
-    Switch::~Switch() {
-        delete activeBackground;
-        delete inactiveBackground;
-    }
+	
+	void Switch::drawDebug(sf::RenderTarget &renderTarget, int indent, int indentAddition, uint hue, uint hueOffset) {
+		if(active->getValue()) {
+			activeBackground->drawDebug(renderTarget, indent, indentAddition, hue, hueOffset);
+		} else {
+			inactiveBackground->drawDebug(renderTarget, indent, indentAddition, hue, hueOffset);
+		}
+	}
+	
+	Switch::~Switch() {
+		delete activeBackground;
+		delete inactiveBackground;
+	}
 }
--- lib/ui/iObject/iUnscalable/iScalable/interactive/simple/switch/switch.hpp
+++ lib/ui/iObject/iUnscalable/iScalable/interactive/simple/switch/switch.hpp
@@ -7,44 +7,44 @@
 #include "../../../uninteractive/uninteractive.hpp"
 #include "../../../../../../enums/keyHandler/keyHandler.hpp"
 
-
-namespace ui{
-    class Switch : public Interactive_Simple, public Layout, public IDrawable{
-    protected:
-        DrawManager activeDrawManager;
-        DrawManager inactiveDrawManager;
-
-        IUninteractive* activeBackground;
-        IUninteractive* inactiveBackground;
-        bool active;
-
-    public:
-        Switch(IUninteractive *inactiveBackground, IUninteractive *activeBackground, Key key = Key::mouseLeft, bool startActive = false);
-
-        void init(InteractiveInitInfo interactiveInitInfo) override;
-
-        void switchValue();
-
-        void setPosition(sf::Vector2f position) override;
-
-        void move(sf::Vector2f position) override;
-
-        void setSize(sf::Vector2f size) override;
-
-        sf::Vector2f getMinSize() override;
-
-        sf::Vector2f getNormalSize() override;
-
-        void draw() override;
-
-        void resize(sf::Vector2f size, sf::Vector2f position) override;
-
-        Switch* copy() override;
-
-        void drawDebug(sf::RenderTarget &renderTarget, int indent, int indentAddition, uint hue, uint hueOffset) override;
-
-        ~Switch();
-    };
+namespace ui {
+	class Switch : public Interactive_Simple, public Layout, public IDrawable {
+	protected:
+		IUninteractive *activeBackground;
+		IUninteractive *inactiveBackground;
+		DrawManager activeDrawManager;
+		DrawManager inactiveDrawManager;
+		PSbool active;
+	
+	public:
+		Switch(IUninteractive *inactiveBackground, IUninteractive *activeBackground, PSbool value, Key key = Key::mouseLeft);
+		
+		Switch(IUninteractive *inactiveBackground, IUninteractive *activeBackground, Key key = Key::mouseLeft, bool startActive = false);
+		
+		void init(InteractiveInitInfo interactiveInitInfo) override;
+		
+		void switchValue();
+		
+		void setPosition(sf::Vector2f position) override;
+		
+		void move(sf::Vector2f position) override;
+		
+		void setSize(sf::Vector2f size) override;
+		
+		sf::Vector2f getMinSize() override;
+		
+		sf::Vector2f getNormalSize() override;
+		
+		void draw() override;
+		
+		void resize(sf::Vector2f size, sf::Vector2f position) override;
+		
+		Switch *copy() override;
+		
+		void drawDebug(sf::RenderTarget &renderTarget, int indent, int indentAddition, uint hue, uint hueOffset) override;
+		
+		~Switch();
+	};
 	
 	bool convertPointer(const YAML::Node &node, Switch *&switcher);
 }
