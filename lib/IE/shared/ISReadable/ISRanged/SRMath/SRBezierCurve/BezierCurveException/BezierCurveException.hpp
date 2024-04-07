6ed044d8288d9b396a2acd0c3de7e4744e8fba20
andrewkoro105 <andrewkoro105@gmail.com>
2024-04-07 16:38:24 +0300

    feat!: change the `SRBezierCurve` constructor to accept points from 0 to 1

--- lib/IE/shared/ISReadable/ISRanged/SRMath/SRBezierCurve/BezierCurveException/BezierCurveException.hpp
+++ lib/IE/shared/ISReadable/ISRanged/SRMath/SRBezierCurve/BezierCurveException/BezierCurveException.hpp
@@ -7,15 +7,16 @@ namespace ie {
 	template<typename T_>
 	class BezierCurveException : public ieml::NodeException{
 	public:
-		BezierCurveException(ieml::Mark mark, std::array<T_, 3> points, bool first_point_not_corrected);
+		BezierCurveException(ieml::Mark mark, size_t i, std::array<T_, 3> points);
+		
+		BezierCurveException(ieml::Mark mark, size_t i, T_ point);
 		
 		auto get_description() const -> std::string override;
 		
 		BezierCurveException<T_>* clone() const override;
 	
 	protected:
-		std::array<T_, 3> points_;
-		bool first_point_not_corrected_;
+		ieml::String str;
 	};
 }
 
